1958e60539e61b842ec5bd31f22178572c78d0d7
凱恩Kane <33802653+Gary50613@users.noreply.github.com>
2022-07-28 08:30:01 +0800

    fix make not installed

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-# check if wget installed first
-sudo apt install wget
+# check if wget & make installed first
+sudo apt update
+sudo apt install make wget -y
 
 # download pre-built btop files
 wget -O btop.tbz https://github.com/aristocratos/btop/releases/latest/download/btop-x86_64-linux-musl.tbz
